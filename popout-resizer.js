b199bfd651dbd6ef4fcb915184a426db8abbf215
Jesse Small <jessesmall95@gmail.com>
2020-06-19 12:10:20 -0600

    Adding support for PF2E

--- popout-resizer.js
+++ popout-resizer.js
@@ -1,7 +1,13 @@
+/**
+ * Static class that handles the sidebar tab rendered hook
+ */
 class PopoutResizer {
     static sidebarTabRendered(obj, html, data) {
+
+        // Only handle this event if the obj in question is a popout
         if(obj.options.popOut){
-            const header = html.find('header')[0];
+
+            // Get existing size data
             let resizeData = obj.resizeData;
 
             var resizablePopout = new ResizablePopout(obj, html);
@@ -15,6 +21,9 @@ class PopoutResizer {
     }
 }
 
+/**
+ * Wrapper class for resizable popout
+ */
 class ResizablePopout {
     constructor(app, html) {
         this.app = app;
@@ -24,14 +33,19 @@ class ResizablePopout {
         this.dragHandler = new Draggable(app, html, header, true);
         
         this.handle = this.html.find('.window-resizable-handle')[0];
-
-        this.handle.addEventListener('mousedown', e => this._onResizeMouseDown(e), false);
+        if(this.handle) {
+            this.handle.addEventListener('mousedown', e => this._onResizeMouseDown(e), false);
+        } else {
+            console.error('Resize handler does not exist on popout for app: ' + this.app.id);
+        }
     }
 
+    // Begin capturing mouse up events to record new window size
     _onResizeMouseDown(event) {
         window.addEventListener('mouseup', e => this._onResizeMouseUp(e), false);
     }
 
+    // On mouse up record window size as the resize event has ended
     _onResizeMouseUp(event) {
         window.removeEventListener('mouseup', e => this._onResizeMouseUp(e), false);
 
@@ -44,4 +58,10 @@ class ResizablePopout {
     }
 }
 
+// Hook into all render sidebar tabs this will fire for every sidebar tab
 Hooks.on("renderSidebarTab", PopoutResizer.sidebarTabRendered);
+
+// Handle combat tracker for PF2E
+if(game.system.id === 'pf2e') {
+    Hooks.on('renderCombatTracker', PopoutResizer.sidebarTabRendered);
+}
